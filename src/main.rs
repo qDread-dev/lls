7b053939eb7da6bfc3e5645a0171f05efc78c091
Evan Fincher <58754377+qDread-dev@users.noreply.github.com>
2023-11-15 19:47:06 -0500

    Added config

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use std::fs::ReadDir;
+use std::fs::File;
+use std::io::Read;
 use term_size;
 use colored::*;
 use clap::Parser;
@@ -16,6 +18,7 @@ struct Args {
     #[arg(short, default_value = "false")]
     unordered: bool,
 }
+
 
 fn sort_dirs(items: ReadDir, sort: bool, all: bool) -> (Vec<String>, Vec<String>) {
     let mut dirs: Vec<String> = Vec::new();
@@ -62,15 +65,45 @@ fn print_vec(vec: Vec<String>, color: Color, bold: bool) {
     }
 }
 
+fn conv_color(color: String) -> Color {
+    match color.as_str() {
+        "red" => Color::Red,
+        "blue" => Color::Blue,
+        "green" => Color::Green,
+        "yellow" => Color::Yellow,
+        "cyan" => Color::Cyan,
+        "magenta" => Color::Magenta,
+        "white" => Color::White,
+        "black" => Color::Black,
+        "bright_red" => Color::BrightRed,
+        "bright_blue" => Color::BrightBlue,
+        "bright_green" => Color::BrightGreen,
+        "bright_yellow" => Color::BrightYellow,
+        "bright_cyan" => Color::BrightCyan,
+        "bright_magenta" => Color::BrightMagenta,
+        "bright_white" => Color::BrightWhite,
+        "bright_black" => Color::BrightBlack,
+        _ => Color::TrueColor {r: color.split(",").nth(0).unwrap().parse::<u8>().unwrap(), g: color.split(",").nth(1).unwrap().parse::<u8>().unwrap(), b: color.split(",").nth(2).unwrap().parse::<u8>().unwrap()},
+    }
+}
+
+fn parse_config() -> json::JsonValue{
+    let mut file = File::open("config.json").unwrap();
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+
+    let json: json::JsonValue = json::parse(&contents).unwrap();
+    return json;
+}
+
 fn main() {
-
+    let config: json::JsonValue = parse_config();
     let args = Args::parse();
-
     let items: ReadDir = std::fs::read_dir(args.path).unwrap();
     
     let (dirs, files) = sort_dirs(items, args.unordered, args.all);
     
-    print_vec(dirs, Color::Blue, true);
+    print_vec(dirs, conv_color(config["dir"]["color"].to_string()), config["dir"]["bold"].as_bool().unwrap());
     println!();
     print_vec(files, Color::White, false);
     println!();
